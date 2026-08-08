85fca42cba16bf61d5036a079d36895ddbe854f3
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2026-08-08 17:41:39 +0530

    Create first React component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,15 @@
+function ProductCard(){
+    return (
+    <div>
+        <h1>laptop prices</h1>
+        <p>Mac book pro-100000</p>
+    </div>
+        
+              
+
+
+
+
+    )
+}   
+export default ProductCard;
